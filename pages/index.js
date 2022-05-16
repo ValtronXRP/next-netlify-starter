c895f7a00357a4420b55ae2b06b943869cc1aeb9
ValtronXRP <103621899+ValtronXRP@users.noreply.github.com>
2022-05-16 15:35:12 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,18 @@ export default function Home() {
         <Header title="Welcome to Genfinity's LightHouse Report" />
         <p className="description">
     
-    <a href="marketsentiment" download="market_sentiment.html">
+    <html>
+<body>
+
+<p>Click on the w3schools logo to download the image:<p>
+
+   <a href="marketsentiment" download="market_sentiment.html">
   <img src="marketsent" alt="W3Schools">
 </a>
+
+</body>
+</html>
+    
           Click here to <code>DOWNLOAD your template</code>
         </p>
       </main>
