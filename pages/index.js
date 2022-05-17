edbbed98ae75815fe2dca239a669706586948d5d
ValtronXRP <103621899+ValtronXRP@users.noreply.github.com>
2022-05-16 18:32:02 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,15 +27,15 @@ export default function Home() {
     
             </p>
     
-       <p className="description">
+    
+      </main>
+    
+    <p className="description">
      
     
           Click here to <code><a href="artemp.pdf" download>Download ADOPTION REVIEW TEMPLATE</a></code>
     
             </p>
-    
-    
-      </main>
 
       <Footer />
     </div>
