c94b20dd8734153ab6f76c1ba34c8560364502e5
ValtronXRP <103621899+ValtronXRP@users.noreply.github.com>
2022-05-16 17:59:59 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
    
    
           Click here to <code><a href="mstemp.pdf" download>Download MARKET SENTIMENT TEMPLATE</a></code>
-        </p>
+       
     
            Click here to <code><a href="artemp.pdf" download>Download ADOPTION REVIEW TEMPLATE</a></code>
          </p>
