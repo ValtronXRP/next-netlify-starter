3b209d9c28c8ebdcb988bba0f98571fef1ae73e3
ValtronXRP <103621899+ValtronXRP@users.noreply.github.com>
2022-05-16 18:12:24 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,12 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
 }
+
+
+
+var desc = "Test\n\nTest"
+desc = desc.replace(/\n/g, "<br>");
+$('#description').html(desc);
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<div id="description"></div>
