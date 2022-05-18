55b9f581c0932a801f54142bcf3448111c1e5843
ValtronXRP <103621899+ValtronXRP@users.noreply.github.com>
2022-05-18 00:59:56 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,35 +31,25 @@ export default function Home() {
   
    <p
           Click here to <a href="mstemp.pdf" download>Download MARKET SENTIMENT TEMPLATE</a>
-    </p>
-    <p
+   
           Click here to <a href="artemp.pdf" download>Download ADOPTION REVIEW TEMPLATE</a>
-     </p>
-    <p
+    
           Click here to <a href="artemp.pdf" download>Download ADOPTION REVIEW TEMPLATE</a>
-     </p>
-    <p
+    
           Click here to <a href="TATEMP.pdf" download>Download TECHNICAL ANALYSIS TEMPLATE</a>
-     </p>
-    <p
+   
           Click here to <a href="TRADFITEMP.pdf" download>Download TRADFI TEMPLATE</a>
-     </p>
-    <p
+    
           Click here to <a href="DEFIWEB3TEMP.pdf" download>Download DEFI WEB3 TEMPLATE</a>
-    </p>
-    <p
+   
           Click here to <a href="ASIANMARKETSTEMP.pdf" download>Download ASIAN MARKETS TEMPLATE</a>
-     </p>
-    <p
+   
           Click here to <a href="REGREVIEWTEMP.pdf" download>Download REGULATION REVIEW TEMPLATE</a>
-     </p>
-    <p
+    
           Click here to <a href="RABBITHOLETEMP.pdf" download>Download RABBIT HOLE TEMPLATE</a>
-     </p>
-    <p
+  
           Click here to <a href="EXECSUITETEMP.pdf" download>Download EXECUTIVE SUITE TEMPLATE</a>
-     </p>
-    <p
+   
           Click here to <a href="CRYPTOEDUCATIONTEMP.pdf" download>Download CRYPTO EDUCATION TEMPLATE</a>
     
             </p>
